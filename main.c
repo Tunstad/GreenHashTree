58e9edad073f1bc87aa4943eaa0190216779cf20
Petter Tunstad <Pettertunstad@gmail.com>
2018-03-20 16:03:26 +0100

    Added simple hash function

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Example Hash Function https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
+unsigned int hash(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
+unsigned int unhash(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = (x >> 16) ^ x;
+    return x;
+}
+
 int main (int argc, char **argv) 
 {
     printf("Running GreenHashTree.. please wait...\n");
